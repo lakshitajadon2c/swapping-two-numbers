efb52b9780bf9eed100a3c48887af7e3c70cc35b
lakshitajadon2c <90979337+lakshitajadon2c@users.noreply.github.com>
2021-09-25 14:18:04 +0530

    create swapping.c
    this file swap two numbers

---
+++ swapping.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a, b;
+printf("enter value for a & b:");
+scanf("%d%d, &a, &b");
+a=a+b;
+b=a-b;
+a=a-b;
+printf("after swapping the value of a & b:%d%d", a,b);
+getch();
+}
